71405ef729a0d56e7830bd1a2e9a41b3bea5576d
Erik E. Lorenz <erik@tuvero.de>
2023-11-15 11:46:07 +0100

    print more job details

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main() {
   long api_version = slurm_api_version();
 
-  std::cout << "SLURM API version: " << api_version << std::endl;
+  std::cout << "SLURM API version: " << api_version << "\n";
 
   job_info_msg_t *job_info_msg_ptr = NULL;
 
@@ -15,27 +15,38 @@ int main() {
   int retval = slurm_load_jobs((time_t)NULL, &job_info_msg_ptr, show_flags);
 
   if (retval != 0) {
-    std::cerr << "slurm_load_jobs() failed: " << retval << std::endl;
+    std::cerr << "slurm_load_jobs() failed: " << retval << "\n";
 
-    std::cerr << "  slurm error: " << slurm_strerror(slurm_get_errno())
-              << std::endl;
+    std::cerr << "  slurm error: " << slurm_strerror(slurm_get_errno()) << "\n";
   }
 
   if (job_info_msg_ptr == nullptr) {
-    std::cerr << "job_info_msg_ptr is null" << std::endl;
+    std::cerr << "job_info_msg_ptr is null"
+              << "\n";
     return 1;
   }
 
   // print number of jobs, last update, and job ids
 
-  std::cout << "Number of jobs: " << job_info_msg_ptr->record_count
-            << std::endl;
-  std::cout << "Last update: " << job_info_msg_ptr->last_update << std::endl;
+  std::cout << "Number of jobs: " << job_info_msg_ptr->record_count << "\n";
+  std::cout << "Last update: " << job_info_msg_ptr->last_update << "\n";
 
   for (size_t i = 0; i < job_info_msg_ptr->record_count; i++) {
-    std::cout << "  Job id: " << job_info_msg_ptr->job_array[i].job_id
-              << std::endl;
+    std::cout << "  Job " << job_info_msg_ptr->job_array[i].job_id << " : "
+              << "\n";
+    // also print the job name, partition, user and cores
+    std::cout << "    Name      : " << job_info_msg_ptr->job_array[i].name
+              << "\n";
+    std::cout << "    Partition : " << job_info_msg_ptr->job_array[i].partition
+              << "\n";
+    std::cout << "    User      : " << job_info_msg_ptr->job_array[i].user_name
+              << " (" << job_info_msg_ptr->job_array[i].user_id << ")"
+              << "\n";
+    std::cout << "    Cores     : " << job_info_msg_ptr->job_array[i].num_cpus
+              << "\n";
   }
+
+  std::cout << std::endl; // flush
 
   // free the job_info_msg_ptr
   slurm_free_job_info_msg(job_info_msg_ptr);
